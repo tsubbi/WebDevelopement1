c1a955c3d849f91ae42a5d08d55478c7f36148c8
Tsubbi <57787652+tsubbi@users.noreply.github.com>
2021-02-16 14:01:26 -0800

    The final project is on CakeShop repo
    https://github.com/tsubbi/CakeShop

--- week6/js/jsonFetcher.js
+++
@@ -1,5 +0,0 @@
-async function fetchJson(url, dataName, callback) {
-    const response = await fetch(url)
-        .then(response => response.json())
-        .then(data => callback(data[dataName]));
-}
